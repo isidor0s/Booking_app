96eb0a33e5b38951f7813a390d444ed3f6e75d9e
BabisDigos <ntigos2001@gmail.com>
2023-05-05 12:05:25 +0300

    Supabase init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { _supabaseClient } from "@/utils/supabase";
+import type { AppProps } from "next/app";
+import { useState } from "react";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [supabaseClient] = useState(() => _supabaseClient);
+
+  return (
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
+      <Component {...pageProps} />
+    </SessionContextProvider>
+  );
 }
