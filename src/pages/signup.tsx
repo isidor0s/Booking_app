7ba598a209151d9649dd3c9c7b62af473627cc93
BabisDigos <ntigos2001@gmail.com>
2023-05-06 17:04:51 +0300

    feat: Sign up as admin or employee

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -5,22 +5,27 @@ import { _supabaseClient } from "@/utils/supabase";
 export default function Signup() {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
-  
-  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const {data,error} = await _supabaseClient.auth.signUp({ 
-        email: email,
-        password: password,
+
+    await _supabaseClient.auth.signUp({
+      email: email,
+      password: password,
+      options: {
+        data: {
+          type: "employee", // admin or employee
+        },
+      },
     });
-     console.log(data,error);
-  }
+  };
 
   return (
     <>
       <div className="flex justify-center items-center h-screen h-screen w-screen">
         <div className=" flex flex-col p-6 space-y-4 md:space-y-6 sm:p-8">
           <h1 className="text-[28px] font-semibold">Create your account</h1>
-          <form className="space-y-4 md:space-y-6 w-80" onSubmit={onSubmit  }>
+          <form className="space-y-4 md:space-y-6 w-80" onSubmit={handleSubmit}>
             <div>
               <label className="block mb-2 text-base font-medium text-[#5A5A5D]">
                 Email
@@ -58,7 +63,6 @@ export default function Signup() {
                   Login
                 </Link>
               </p>
-              
             </div>
           </form>
         </div>
