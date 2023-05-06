6a165c6e8a7a124a0b7b893cf45ccc0408385f2d
isidor0s <isidoroschatz@gmail.com>
2023-05-06 15:25:55 +0300

    sign-up functionality

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -3,21 +3,31 @@ import Link from "next/link";
 import { _supabaseClient } from "@/utils/supabase";
 
 export default function Signup() {
-  const [username, setUsername] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const {data,error} = await _supabaseClient.auth.signUp({ 
+        email: email,
+        password: password,
+    });
+     console.log(data,error);
+  }
+
   return (
     <>
       <div className="flex justify-center items-center h-screen h-screen w-screen">
         <div className=" flex flex-col p-6 space-y-4 md:space-y-6 sm:p-8">
           <h1 className="text-[28px] font-semibold">Create your account</h1>
-          <form className="space-y-4 md:space-y-6 w-80">
+          <form className="space-y-4 md:space-y-6 w-80" onSubmit={onSubmit  }>
             <div>
               <label className="block mb-2 text-base font-medium text-[#5A5A5D]">
                 Email
               </label>
               <input
                 type="email"
-                onChange={(e) => setUsername(e.target.value)}
+                onChange={(e) => setEmail(e.target.value)}
                 className="text-black text-sm border border-slate-400 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  "
                 placeholder="example@gmail.com"
                 required
@@ -48,6 +58,7 @@ export default function Signup() {
                   Login
                 </Link>
               </p>
+              
             </div>
           </form>
         </div>
