5618ec1892abfe797307cdc8daec6ffa74764bec
BabisDigos <ntigos2001@gmail.com>
2023-05-06 17:28:47 +0300

    feat: Added eslint rules, prettier format, tailwind format

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,25 @@
-import "@/styles/globals.css";
-import { _supabaseClient } from "@/utils/supabase";
-import type { AppProps } from "next/app";
-import { useState } from "react";
+import '@/styles/globals.css';
+import { _supabaseClient } from '@/utils/supabase';
+import type { AppProps } from 'next/app';
+import { useState } from 'react';
 import { Inter } from 'next/font/google';
-import { SessionContextProvider } from "@supabase/auth-helpers-react";
-const inter = Inter({ subsets: ['latin'] });
-export default function App({ Component, pageProps }: AppProps) {
-  const [supabaseClient] = useState(() => _supabaseClient);
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
 
-  return (
-    <SessionContextProvider
-      supabaseClient={supabaseClient}
-      initialSession={pageProps.initialSession}
-    >
-      <Component {...pageProps} className={`${inter.className}`}/>
-    </SessionContextProvider>
-  );
+const inter = Inter({
+    weight: ['400', '500', '600', '700', '800'],
+    display: 'swap',
+    variable: '--primaryFont',
+    subsets: ['latin'],
+});
+
+export default function App({ Component, pageProps }: AppProps) {
+    const [supabaseClient] = useState(() => _supabaseClient);
+
+    return (
+        <main className={inter.className}>
+            <SessionContextProvider supabaseClient={supabaseClient} initialSession={pageProps.initialSession}>
+                <Component {...pageProps} />
+            </SessionContextProvider>
+        </main>
+    );
 }
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,72 +1,65 @@
-import { useState } from "react";
-import Link from "next/link";
-import { _supabaseClient } from "@/utils/supabase";
+import { useState } from 'react';
+import Link from 'next/link';
+import { _supabaseClient } from '@/utils/supabase';
 
 export default function Signup() {
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
 
-    await _supabaseClient.auth.signUp({
-      email: email,
-      password: password,
-      options: {
-        data: {
-          type: "employee", // admin or employee
-        },
-      },
-    });
-  };
+        await _supabaseClient.auth.signUp({
+            email: email,
+            password: password,
+            options: {
+                data: {
+                    type: 'employee', // admin or employee
+                },
+            },
+        });
+    };
 
-  return (
-    <>
-      <div className="flex justify-center items-center h-screen h-screen w-screen">
-        <div className=" flex flex-col p-6 space-y-4 md:space-y-6 sm:p-8">
-          <h1 className="text-[28px] font-semibold">Create your account</h1>
-          <form className="space-y-4 md:space-y-6 w-80" onSubmit={handleSubmit}>
-            <div>
-              <label className="block mb-2 text-base font-medium text-[#5A5A5D]">
-                Email
-              </label>
-              <input
-                type="email"
-                onChange={(e) => setEmail(e.target.value)}
-                className="text-black text-sm border border-slate-400 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  "
-                placeholder="example@gmail.com"
-                required
-              ></input>
+    return (
+        <>
+            <div className="flex h-screen w-screen items-center justify-center">
+                <div className=" flex flex-col space-y-4 p-6 sm:p-8 md:space-y-6">
+                    <h1 className="text-[28px] font-semibold">Create your account</h1>
+                    <form className="w-80 space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+                        <div>
+                            <label className="mb-2 block text-base font-medium text-[#5A5A5D]">Email</label>
+                            <input
+                                type="email"
+                                onChange={(e) => setEmail(e.target.value)}
+                                className="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-slate-400 p-2.5 text-sm text-black"
+                                placeholder="example@gmail.com"
+                                required
+                            ></input>
+                        </div>
+                        <div>
+                            <label className="mb-2 block text-base font-medium text-[#5A5A5D]">Password</label>
+                            <input
+                                type="password"
+                                onChange={(e) => setPassword(e.target.value)}
+                                className="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-slate-400 p-2.5 text-sm text-black"
+                                placeholder="Enter your password"
+                                required
+                            ></input>
+                        </div>
+                        <button className=" block w-full rounded-lg bg-[#1E293B] p-2.5 text-white focus:border-[#1E293B] focus:ring-[#1E293B]">
+                            Sign up
+                        </button>
+                        <div className="flex justify-center">
+                            <p className="text-sm font-light text-gray-500">
+                                Already have an account?{' '}
+                                <Link href="/login" className="font-medium text-[#1E293B] hover:underline ">
+                                    Login
+                                </Link>
+                            </p>
+                        </div>
+                    </form>
+                </div>
             </div>
-            <div>
-              <label className="block mb-2 text-base font-medium text-[#5A5A5D]">
-                Password
-              </label>
-              <input
-                type="password"
-                onChange={(e) => setPassword(e.target.value)}
-                className="text-black text-sm border border-slate-400 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  "
-                placeholder="Enter your password"
-                required
-              ></input>
-            </div>
-            <button className=" text-white bg-[#1E293B] rounded-lg focus:ring-[#1E293B] focus:border-[#1E293B] block w-full p-2.5  ">
-              Login
-            </button>
-            <div className="flex justify-center">
-              <p className="text-black text-sm font-light text-gray-500">
-                Already have an accoount?{" "}
-                <Link
-                  href="/login"
-                  className="font-medium text-[#1E293B] hover:underline "
-                >
-                  Login
-                </Link>
-              </p>
-            </div>
-          </form>
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 }
