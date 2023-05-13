b66df8dd9d3b125a8dd9771a3644143ad8f59977
BabisDigos <ntigos2001@gmail.com>
2023-05-13 18:50:51 +0300

    feat: Create room

--- src/pages/admin/create-room.tsx
+++ src/pages/admin/create-room.tsx
@@ -1,58 +1,117 @@
 import Layout from '@/layouts/layout';
+import { Room } from '@/types/room';
+import { _supabaseClient } from '@/utils/supabase';
+import { useUser } from '@supabase/auth-helpers-react';
 import { NextPage } from 'next';
+import { FormEvent, useRef, useState } from 'react';
 
 const CreateRoom: NextPage = () => {
+    const user = useUser();
+
+    const formRef = useRef<HTMLFormElement>(null);
+
+    const [name, setName] = useState('');
+    const [type, setType] = useState('');
+    const [capacity, setCapacity] = useState(0);
+    const [date, setDate] = useState(new Date().toISOString());
+
+    const [success, setSuccess] = useState(false);
+
+    const resetForm = () => {
+        if (formRef.current) formRef.current.reset();
+    };
+
+    const createRoom = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const { data } = await _supabaseClient
+            .from('room')
+            .insert({
+                admin_id: user?.id,
+                name: name,
+                date: date,
+                type: type,
+                capacity: capacity,
+                availability: 'Available',
+            })
+            .select()
+            .single<Room>();
+
+        resetForm();
+
+        if (data) {
+            setSuccess(true);
+            setTimeout(() => setSuccess(false), 5000);
+        }
+    };
+
     return (
         <Layout>
             <div className={'text-3xl font-semibold text-slate-800'}>Create room</div>
-            <div>
-                <form className="flex flex-col gap-6">
-                    <div>
-                        <label className="mb-2 block text-xl font-medium text-slate-800">Name</label>
+            <form ref={formRef} className={'flex flex-col gap-6'} onSubmit={createRoom}>
+                <div className={'flex w-48 flex-col gap-1.5'}>
+                    <label htmlFor={'name'} className={'w-fit text-base font-medium text-slate-700'}>
+                        Name
+                    </label>
+                    <input
+                        id={'name'}
+                        type="text"
+                        className={'rounded-lg border border-slate-400 p-2.5 text-sm text-slate-900'}
+                        placeholder={'e.g. Auditorium'}
+                        onChange={(e) => setName(e.target.value)}
+                        required
+                    ></input>
+                </div>
+                <div className={'flex w-48 flex-col gap-1.5'}>
+                    <label htmlFor={'date'} className={'w-fit text-base font-medium text-slate-700'}>
+                        Date
+                    </label>
+                    <input
+                        id={'date'}
+                        type="datetime-local"
+                        className={'rounded-lg border border-slate-400 p-2.5 text-sm text-slate-900'}
+                        onChange={(e) => setDate(e.target.value)}
+                        required
+                    ></input>
+                </div>
+                <div className={'flex w-72 flex-row gap-6'}>
+                    <div className={'flex w-2/3 flex-col gap-1.5'}>
+                        <label htmlFor={'type'} className={'w-fit text-base font-medium text-slate-700'}>
+                            Type
+                        </label>
                         <input
-                            // onChange={(e) => setEmail(e.target.value)}
-                            className="focus:ring-primary-600 focus:border-primary-600 block w-48 rounded-lg border border-slate-400 p-2.5 text-base text-black  "
-                            placeholder="e.g. Auditorium"
+                            id={'type'}
+                            type="text"
+                            className={'rounded-lg border border-slate-400 p-2.5 text-sm text-slate-900'}
+                            placeholder={'e.g. Course'}
+                            onChange={(e) => setType(e.target.value)}
                             required
                         ></input>
                     </div>
-                    <div>
-                        <label className="mb-2 block text-xl font-medium text-slate-800">Date</label>
+                    <div className={'flex w-1/3 flex-col gap-1.5'}>
+                        <label htmlFor={'capacity'} className={'w-fit text-base font-medium text-slate-700'}>
+                            Capacity
+                        </label>
                         <input
-                            type="Date"
-                            // onChange={(e) => setEmail(e.target.value)}
-                            className="focus:ring-primary-600 focus:border-primary-600 block w-48 rounded-lg border border-slate-400 p-2.5 text-base text-black  "
-                            placeholder="mm/dd/yyyy"
+                            id={'capacity'}
+                            type="number"
+                            className={'rounded-lg border border-slate-400 p-2.5 text-sm text-slate-900'}
+                            onChange={(e) => setCapacity(parseInt(e.target.value))}
                             required
                         ></input>
                     </div>
-                    <div>
-                        <div className="flex gap-6">
-                            <div>
-                                <label className="mb-2 block text-xl font-medium text-slate-800">Date</label>
-                                <input
-                                    // onChange={(e) => setEmail(e.target.value)}
-                                    className="focus:ring-primary-600 focus:border-primary-600 block w-48 rounded-lg border border-slate-400 p-2.5 text-base text-black  "
-                                    placeholder="e.g. Course"
-                                    required
-                                ></input>
-                            </div>
-                            <div>
-                                <label className="mb-2 block text-xl font-medium text-slate-800">Capacity</label>
-                                <input
-                                    // onChange={(e) => setEmail(e.target.value)}
-                                    className="focus:ring-primary-600 focus:border-primary-600 block w-24 rounded-lg border border-slate-400 p-2.5 text-base text-black  "
-                                    placeholder="e.g. 12"
-                                    required
-                                ></input>
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                    <div><button className="rounded-lg border bg-slate-800 px-4 py-3 text-white transition-colors hover:border-slate-500 hover:bg-slate-100 hover:text-slate-500 text-base">Create</button></div>
-                    </div>
-                </form>
-            </div>
+                </div>
+                <div className={'flex flex-row items-center gap-6'}>
+                    <button
+                        type="submit"
+                        className={
+                            'w-fit rounded-lg bg-slate-800 p-2.5 text-white focus:border-slate-800 focus:ring-slate-800'
+                        }
+                    >
+                        Create
+                    </button>
+                    {success && <div className={'text-sm font-medium text-green-500'}>Room created successfully!</div>}
+                </div>
+            </form>
         </Layout>
     );
 };
